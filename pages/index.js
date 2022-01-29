fc0b22114e66229abb8d1e48bc173f21ac0798d3
Sebastião Marcos <sebastiaomarcos49@gmail.com>
2022-01-29 09:07:03 -0300

    Add images

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
 import Link from "next/link";
+import Image from "next/image";
 
 
 function HomePage() {
+  const MyComponent = () => (
+    <Image src="/images/sebas.jpg" // Route of the image file
+      height={144} // Desired size with correct aspect ratio
+      width={144} // Desired size with correct aspect ratio
+      alt="Sebastião Marcos"
+    />
+
+  )
+
   return (
-  <h1 className="title">
-  Read{' '}
-  <Link href="/posts/first-post">
-    <a>this page!</a>
-  </Link>
-</h1>
+    <>
+      <MyComponent />
+      <h1 className="title">
+
+        Read{' '}
+
+        <Link href="/posts/first-post">
+          <a>this page!</a>
+        </Link>
+      </h1>
+    </>
   )
 }
 
